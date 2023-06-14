91f595bd5c162a247ca96375f8ee2297e9ad4b3e
Innaias <innaias@harveyosity.org>
2023-06-14 15:50:15 -0700

    Made the grappling hook unable to go through objects if there is a grappleable object in the way by regrappling to the nearest object. Terrain sets are 100% working now. Fixed the tile set a little

--- player.gd
+++ player.gd
@@ -27,6 +27,7 @@ var coyote_time := COYOTE_TIME
 var jumped := false
 
 var grapple_state := GrappleState.NOT_GRAPPLED
+var grapple_centered := false
 var grapple_position := Vector2.ZERO
 
 var grapple_query: PhysicsRayQueryParameters2D
@@ -67,29 +68,37 @@ func _physics_process(delta):
 	velocity.x += (0.0 - velocity.x) * delta * (GROUND_FRICTION if on_floor else AIR_FRICTION)
 	
 	# Now do grappling hook physics
-	
 	var tile_size = TILEMAP.tile_set.tile_size
 	var grapple_collision_coords := Vector2i(-1, -1)
 	if $GrappleRaycast.is_colliding():
 		grapple_collision_coords = TILEMAP.get_coords_for_body_rid($GrappleRaycast.get_collider_rid())
-	var grapple_collision_tile_data := TILEMAP.get_cell_tile_data(0, grapple_collision_coords)
-	var grapple_is_colliding = $GrappleRaycast.is_colliding() and grapple_collision_tile_data.get_custom_data("grappleable")
-	$Label.text = ""
+	var grapple_collision_tile_data := TILEMAP.get_cell_tile_data(1, grapple_collision_coords)
+	var grapple_is_colliding := false
+	grapple_is_colliding = $GrappleRaycast.is_colliding() and grapple_collision_tile_data.get_custom_data("grappleable")
+
 	if Input.is_action_just_pressed("grapple") and grapple_is_colliding:
 		grapple_state = GrappleState.ANIMATING_TO
 		if grapple_collision_tile_data.get_custom_data("snap_to_center"):
+			grapple_centered = true
 			grapple_position = Vector2(grapple_collision_coords * tile_size) + Vector2(tile_size) * 0.5
 		else:
+			grapple_centered = false
 			grapple_position = $GrappleRaycast.get_collision_point()
+		
 		$GrappleAnimationTimer.start()
 	elif Input.is_action_just_released("grapple") and not grapple_state == GrappleState.NOT_GRAPPLED:
 		grapple_state = GrappleState.ANIMATING_FROM
+	
 		$GrappleAnimationTimer.start()
+	
+	if grapple_is_colliding and not grapple_centered:
+		if $GrappleRaycast.get_collision_point().distance_squared_to(position) < grapple_position.distance_squared_to(position):
+			grapple_position = $GrappleRaycast.get_collision_point()
 
 	var grapple_direction: Vector2 = ((position + Vector2.ONE * SIZE * 0.5) - grapple_position)
 	grapple_direction = grapple_direction.normalized() * min(1.0, grapple_direction.length())
 	
-	if grapple_state == GrappleState.ANIMATING_TO or grapple_state == GrappleState.GRAPPLED:
+	if not grapple_state == GrappleState.NOT_GRAPPLED:
 		velocity -= grapple_direction * GRAPPLE_STRENGTH * delta
 	
 	velocity = velocity.limit_length(MAX_VELOCITY)
